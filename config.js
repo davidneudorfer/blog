3597932dc7f413ccba79e928d3fd0ca5c6d4bc8f
David Vasandani <david@davidneudorfer.com>
2019-02-19 07:42:33 +0000

    disable blog posts and update name and links

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
+  url: 'https://www.vasaandani.me',
+  title: 'Blog by David Vasandani',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-67832556-1',
   menu: [
     {
       label: 'Articles',
@@ -23,16 +23,14 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'David Vasandani',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      email: 'david@vasandani.me',
+      twitter: 'davidvasandani',
+      instagrma: 'davidvasandani',
+      github: 'davidneudorfer'
     }
   }
 };
